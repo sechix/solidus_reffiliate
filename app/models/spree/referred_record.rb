4c73634c4f339ea5d0b670aeee7ccbb10e1a4955
Peter Abrahamsen <rainhead@gmail.com>
2017-02-13 16:39:59 -0800

    Don't destroy user when destroying referred_record

--- app/models/spree/referred_record.rb
+++ app/models/spree/referred_record.rb
@@ -1,7 +1,7 @@
 module Spree
   class ReferredRecord < Spree::Base
     belongs_to :referral
-    belongs_to :user, class_name: Spree.user_class.to_s, dependent: :destroy
+    belongs_to :user, class_name: Spree.user_class.to_s
     belongs_to :affiliate
 
     validates_presence_of :user

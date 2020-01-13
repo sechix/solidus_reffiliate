03ee4b91feb97533d790661a5f5dd30ae25be4a3
sechix <sechix@gmail.com>
2020-01-13 14:19:57 +0100

    Update show.rb

--- app/overrides/spree/users/show.rb
+++ app/overrides/spree/users/show.rb
@@ -5,7 +5,12 @@ Deface::Override.new(
 ) do
 <<-CODE.chomp
 <dt>Referral URL</dt>
-<dd><input type='text' value='<%= referral_url(@user.referral.code) %>' onClick='this.select();' /></dd>
+<dd><input type='text' value='<%= referral_url(@user.referral.code) %>' onClick='this.select();' id='copy_text_url' /><br>
+<!-- Trigger -->
+<button class="clipboard-btn" data-clipboard-action="copy" data-clipboard-target="#copy_text_url">
+    Copy to clipboard
+</button>
+</dd>
 <dt>Referred Users</dt>
 <dd><%= @user.referred_count%></dd>
 CODE

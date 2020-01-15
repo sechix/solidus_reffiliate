b1105db8cf9cbfcde55ab6109040359aad0903ee
sechix <sechix@gmail.com>
2020-01-15 15:58:31 +0100

    Update affiliated_promotion_rule.rb

--- app/models/spree/promotion/rules/affiliated_promotion_rule.rb
+++ app/models/spree/promotion/rules/affiliated_promotion_rule.rb
@@ -2,8 +2,8 @@ module Spree
   class Promotion
     module Rules
       class AffiliatedPromotionRule < Spree::PromotionRule
-        belongs_to :affiliate, :class_name => '::Spree::Affiliate'
-        has_and_belongs_to_many :affiliates, :class_name => '::Spree::Affiliate',
+        belongs_to :affiliate, :class_name => 'Spree::Affiliate'
+        has_and_belongs_to_many :affiliates, :class_name => 'Spree::Affiliate',
           :join_table => 'spree_affiliates_promotion_rules', :foreign_key => 'promotion_rule_id',
           :association_foreign_key => 'affiliate_id'
 

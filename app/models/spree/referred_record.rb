5f10aaec2745e2f89631c6106378ff2c9f20e9cf
sechix <sechix@gmail.com>
2020-01-16 11:20:51 +0100

    Update referred_record.rb

--- app/models/spree/referred_record.rb
+++ app/models/spree/referred_record.rb
@@ -1,8 +1,8 @@
 module Spree
   class ReferredRecord < Spree::Base
-    belongs_to :referral
+    belongs_to :referral, optional: true
     belongs_to :user, class_name: Spree::UserClassHandle.new
-    belongs_to :affiliate
+    belongs_to :affiliate, optional: true
 
     validates_presence_of :user
   end

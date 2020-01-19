a0c52d02c2757da52d9524a28562f14d5566a3fc
sechix <sechix@gmail.com>
2020-01-19 13:35:54 +0100

    Update referred_record.rb

--- app/models/spree/referred_record.rb
+++ app/models/spree/referred_record.rb
@@ -17,6 +17,7 @@ module Spree
                                created_by: self.referral.user,
                                action_originator: self,
                                category: store_credit_category,
+                               type: Spree::StoreCreditType.find_by(name: 'Non-expiring')
                            })
     end
 
